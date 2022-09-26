9513933847974e891b0d1a4c0a8dfd82db8cfc94
Seongsan Hong <skhu201622077@gmail.com>
2022-09-26 15:44:03 +0900

    Main.java 수정 #3

--- src/d201622077/Main.java
+++ src/d201622077/Main.java
@@ -1,12 +1,9 @@
 package d201622077;
 
 public class Main {
-    static void say(String s) {
-		System.out.println(s);
-	}
-	
 	public static void main(String [] args) {
-		say("hello world");
+		String s = "안녕하세요";
+		System.out.println(s);
 	}
 
 }
